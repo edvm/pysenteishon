af6f1fff7aa33d7b2e187139eaba2800dbf5fd03
Emiliano Dalla Verde Marcozzi <edvm@msa.com.ar>
2014-10-04 09:23:21 -0300

    improved js

--- src/static/pysenteishon.js
+++ src/static/pysenteishon.js
@@ -4,32 +4,13 @@ $(function() {
     //Generic swipe handler for all directions
     swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
       $(this).text("You swiped " + direction );  
-      if (direction == 'right'){
-          $.ajax({
-            type: 'GET',
-            url: '/btn-right/',
-          });
-      }
-      if (direction == 'left'){
-          $.ajax({
-            type: 'GET',
-            url: '/btn-left/',
-          });
-      }
-      if (direction == 'up'){
-          $.ajax({
-            type: 'GET',
-            url: '/btn-up/',
-          });
-      }
-      if (direction == 'down'){
-          $.ajax({
-            type: 'GET',
-            url: '/btn-down/',
-          });
-      }
+      var url = '/btn-' + direction + '/'
+      $.ajax({
+        type: 'GET',
+        url: url, 
+      });
     },
     //Default is 75px, set to 0 for demo so any distance triggers swipe
-      threshold:0
+    threshold:0
   });
 });
