82063ab517834b22bc79cd2e054c765a804141a6
Emiliano Dalla Verde Marcozzi <edalla@fedoraproject.org>
2014-10-07 00:35:21 -0300

    added cx_freeze script to make gnu/linux friendly packages

---
+++ freeze.py
@@ -0,0 +1,22 @@
+from cx_Freeze import setup, Executable
+
+packages = ['jinja2']
+build_exe_options = {
+    "packages": packages,
+    "include_files": [
+        ('src/static/', 'static'),
+        ('src/templates/', 'templates'),
+    ]
+}
+executables = [
+    Executable('src/web.py',
+               targetName='pysenteishon',
+               )
+]
+
+setup(name='pysenteishon',
+      version='0.1',
+      options={'build_exe': build_exe_options},
+      description='Control your slides with your smartphone touchscreen',
+      executables=executables
+      )
