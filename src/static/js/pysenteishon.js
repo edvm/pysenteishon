6dbd82ab6edba6e0077ccd52289e7b3aeabc5302
Andres Pardini <unpardos@gmail.com>
2015-11-14 08:50:01 -0300

    Improved the chronometer

--- src/static/js/pysenteishon.js
+++ src/static/js/pysenteishon.js
@@ -1,84 +1,53 @@
-// chronometer / stopwatch JS script - coursesweb.net
-// Here set the minutes, seconds, and tenths-of-second when you want the chronometer to stop
-// If all these values are set to 0, the chronometer not stop automatically
-var stmints = 0;
-var stseconds = 0;
-var stzecsec = 0;
+function Chronometer ($el) {
+  this.$el = $el;
+  this.$timeLeft = this.$el.find(".time-left");
+  this.$clock = this.$el.find(".clock");
+  this.running = false;
+  this.lastCheckTime = null;
+  this.milliSecondsLeft = 45*60*1000;
 
-var chrRunning = false;
-
-// the initial tenths-of-second, seconds, and minutes
-var zecsec = 0;
-var seconds = 0;
-var mints = 0;
-var startchron = 0;
-
-// function to be executed when the chronometer stops
-function toAutoStop() {
-  "use strict";
-  console.log("toAutoStop");
-}
-
-function chronometer() {
-  "use strict";
-  var timeoutID;
-
-  if (startchron == 1) {
-    zecsec += 1;       // set tenths of a second
-
-    // set seconds
-    if (zecsec > 9) {
-      zecsec = 0;
-      seconds += 1;
+  this.$clock.swipe({
+    tap: function (event, object) {
+      this.running = !this.running;
+      if (this.running) {
+        this.$timeLeft.css("color", "green");
+        this.lastCheckTime = new Date();
+      } else {
+        this.$timeLeft.css("color", "red");
+      }
+    }.bind(this),
+    doubleTap: function(event, object) {
     }
+  });
 
-    // set minutes
-    if (seconds > 59) {
-      seconds = 0;
-      mints += 1;
+  this.$timeLeft.click(function(){
+    var minutes = prompt("Time in minutes");
+    this.milliSecondsLeft = minutes*60*1000;
+    this.lastCheckTime = new Date();
+    this.running = false;
+    this.render();
+  }.bind(this));
+
+  setInterval(function () {
+    if (this.running) {
+      this.milliSecondsLeft -= ((new Date()).getTime() - this.lastCheckTime.getTime());
+      this.lastCheckTime = new Date();
+      this.render();
     }
+  }.bind(this), 500);
 
-    // adds data in #showtm
-    document.getElementById("showtm").innerHTML = mints + " : " + seconds + "<sub>" + zecsec + "</sub>";
+  this.render = function () {
+    var min = (this.milliSecondsLeft/1000/60) << 0;
+    var sec = (this.milliSecondsLeft/1000) % 60 << 0;
+    this.$timeLeft.text(min+":"+sec);
+  };
 
-    // if the chronometer reaches to the values for stop, calls whenChrStop(), else, auto-calls chronometer()
-    if (zecsec == stzecsec && seconds == stseconds && mints == stmints) {
-      toAutoStop();
-    } else {
-      timeoutID = window.setTimeout(chronometer, 100);
-    }
-  }
+  this.render();
 }
-
-function startChr() {
-  "use strict";
-  chrRunning = true;
-  startchron = 1;
-  chronometer();  // starts the chronometer
-}
-
-function stopChr() {
-  "use strict";
-  chrRunning = false;
-  startchron = 0;  // stops the chronometer
-}
-
-function chrIsRunning(){
-  "use strict";
-  return chrRunning;
-}
-
-function resetChr() {
-  "use strict";
-  zecsec = seconds = mints = startchron = 0;
-  document.getElementById("showtm").innerHTML = mints + " : " + seconds + "<sub>" + zecsec + "</sub>";
-}
-
-// start the chronometer, delete this line if you want to not automatically start the stopwatch
-// startChr();
-
 
 $(function() {
+  new Chronometer($("#chronometer"));
+
   // Enable swiping...
   $("#touche").swipe({
     //Generic swipe handler for all directions
@@ -116,16 +85,4 @@ $(function() {
     threshold:0
   });
 
-  $(".chronometer").swipe({
-    tap: function(event, object){
-      if (chrIsRunning() === false) {
-        startChr();
-      }
-    },
-
-    doubleTap: function(event, object) {
-      resetChr();
-      stopChr();
-    },
-  });
 });
